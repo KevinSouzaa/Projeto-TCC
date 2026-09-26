4c2174c9fef39e674e337e57e2ee6e73355b5721
Kevin <Kevinsouzakleinschmidt44@gmail.com>
2026-09-26 17:56:18 -0300

    Menu abre e fecha e animações do boneco a cada clique

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 let gem = document.querySelector('.gem-cost')
 
+const personagemImg = document.querySelector('.boneco-image');
+
+const imgNormal = './foto/Personagem Fraco-1.png';
+const imgClicado = "./foto/Personagem Fraco-2.png";
+
+let tempoAnimacao;
+
+// Troca as imagens do personagem ao clicar
+personagemImg.addEventListener("click", function(){
+    personagemImg.src = imgClicado;
+
+    // 2. Reseta o tempo caso o jogador clique muito rápido
+    //clearTimeout(tempoAnimacao);
+
+    tempoAnimacao = setTimeout(() => {
+        personagemImg.src = imgNormal;
+    }, 150);
+})
+
 let clickerCost = document.querySelector('.clicker-preco')
 let parseClickerCost = parseFloat(clickerCost.innerHTML)
+
+const btn_AbrirConfig = document.getElementById("bot-direita")
+const btn_fecharConfig = document.getElementById("fechar-config");
+
+const boxconfig = document.getElementById("configuracoes")
+const overlayConfig = document.getElementById('overlay-configuracoes');
+
+
 
 function incrementGem() {
     gem.innerHTML = parseFloat(gem.innerHTML) + 1
@@ -13,4 +40,21 @@ function comprarFrango() {
     }
     
 }
+
+function abrirMenu(){
+    boxconfig.classList.add("mostrar");
+    overlayConfig.classList.add("mostrar")
+    
+}
+    
+
+function fecharMenu(){
+    boxconfig.classList.remove("mostrar");
+    overlayConfig.classList.remove("mostrar")
+}
+
+btn_AbrirConfig.addEventListener("click", abrirMenu);
+btn_fecharConfig.addEventListener("click", fecharMenu);
+    
+
 
